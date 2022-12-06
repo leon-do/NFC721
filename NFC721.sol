4dde00ef2cd9bed1dbc7018f30d707a299e226eb
Leon Do <leondo123@gmail.com>
2022-12-06 16:54:19 -0500

    Update NFC721.sol

--- NFC721.sol
+++ NFC721.sol
@@ -2,28 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFC721 is ERC721URIStorage {
+contract ERC721Token is ERC721URIStorage {
     address public owner;
-
-    using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
 
     constructor() ERC721("Token721", "TKN721") {
         owner = msg.sender;
     }
 
-    function ownerMint(address _to, string memory _tokenURI) public returns(uint256) {
+    function ownerMint(address _to, uint256 _tokenId, string memory _tokenURI) public returns(uint256) {
         require(msg.sender == owner, "Only owner can mint");
-        tokenIds.increment();
-        uint256 newItemId = tokenIds.current();
-        _mint(_to, newItemId);
-        _setTokenURI(newItemId, _tokenURI);
-        return newItemId;
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _tokenURI);
+        return _tokenId;
     }
 
-    function transferOwner(address _owner) public {
+    function ownerTransfer(address _owner) public {
         require(msg.sender == owner, "Only owner can transfer");
         owner = _owner;
     }
