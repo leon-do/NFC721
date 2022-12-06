f899725b52c98ace08ae53a0455a98300e001d6d
Leon Do <leondo123@gmail.com>
2022-12-06 16:59:44 -0500

    Update NFC721.sol

--- NFC721.sol
+++ NFC721.sol
@@ -6,15 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract ERC721Token is ERC721URIStorage {
     address public owner;
 
+    mapping (uint256 => string) public username;
+
     constructor() ERC721("Token721", "TKN721") {
         owner = msg.sender;
     }
 
-    function ownerMint(address _to, uint256 _tokenId, string memory _tokenURI) public returns(uint256) {
+    function ownerMint(address _to, uint256 _tokenId, string memory _username, string memory _tokenURI) public {
         require(msg.sender == owner, "Only owner can mint");
         _mint(_to, _tokenId);
         _setTokenURI(_tokenId, _tokenURI);
-        return _tokenId;
+        username[_tokenId] = _username;
     }
 
     function ownerTransfer(address _owner) public {
